c99f7b56c0d481fb9af052f460081bbff12a6904
Jeriko316 <jkenriquez@hotmail.com>
2023-12-04 18:34:06 -0500

    ejercicio

---
+++ src/main/java/com/uce/edu/EjercicioPa2U1P5Je3Application.java
@@ -0,0 +1,13 @@
+package com.uce.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioPa2U1P5Je3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioPa2U1P5Je3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/EjercicioPa2U1P5Je3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioPa2U1P5Je3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
